d12ba05a8590b92155d6c8ea466cbcbd79be2c04
Thism2 <thism.rasse@hotmail.com>
2015-08-04 14:30:29 +0200

    Started with main menu code

---
+++ Assets/Scripts/Game Menu/Level.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level : MonoBehaviour {
+
+	public enum LevelType
+	{
+		Tutorial,
+		Level,
+		BonusReward
+	}
+	public LevelType mLevelType;
+	public string mLevelName;
+	public int mTotalDistance;
+
+	private bool mUnlocked;
+	private	TextMesh mTitleText;
+	private	TextMesh mTotalDistanceText;
+
+	// Use this for initialization
+	void Start () 
+	{
+		mUnlocked = false;
+
+		mTitleText = transform.Find ("Title").GetComponent<TextMesh> ();
+		mTotalDistanceText = transform.Find ("Dsitance").GetComponent<TextMesh> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+	public bool Unlock()
+	{
+		if (!mUnlocked)
+		{
+			mUnlocked = true;
+			return true;
+		}
+
+		mUnlocked = true;
+		return false;
+	}
+
+	public bool IsUnlocked()
+	{
+		return mUnlocked;
+	}
+
+	public bool Lock()
+	{
+		if (mUnlocked)
+		{
+			mUnlocked = false;
+			return true;
+		}
+		
+		mUnlocked = true;
+		return false;
+	}
+}
