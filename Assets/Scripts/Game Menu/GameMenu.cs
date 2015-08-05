1a5439fc27ba48ba1f522c6920650a303e024be6
Thism2 <thism.rasse@hotmail.com>
2015-08-05 16:16:12 +0200

    Added Items code.

--- Assets/Scripts/Game Menu/GameMenu.cs
+++ Assets/Scripts/Game Menu/GameMenu.cs
@@ -4,11 +4,22 @@ using System.Collections;
 public class GameMenu : MonoBehaviour 
 {
 	public GameObject[] mMenuPositions;
+	public GameObject mStartMenuPosition;
+	public MenuCamera mMenuCamera;
 
 	// Use this for initialization
 	void Start () 
 	{
-	
+		mMenuCamera = GameObject.Find ("Menu Camera").GetComponent<MenuCamera>();
+
+		if (mStartMenuPosition == null)
+		{
+			mStartMenuPosition = mMenuPositions[0];
+		}
+
+		mMenuCamera.transform.position = mStartMenuPosition.transform.position;
+
+		transform.Find ("World Map 0x y0 230z").GetComponent<WorldMapMenu> ().Init();
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/Game Menu/Perks/AirPerk.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirPerk : Perk 
+{
+	public string mPerkName;
+	
+	private bool mMainUnlocked;
+	private bool mLeftUnlocked;
+	private bool mRightUnlocked;
+	private	TextMesh mTitleText;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	public override void Init()
+	{
+		if (mPerkName.Length < 1)
+		{
+			mPerkName = gameObject.name;
+		}
+		
+		mTitleText = transform.Find ("Air/Air+ text").GetComponent<TextMesh> ();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		mTitleText.text = mPerkName;
+	}
+	
+	public override bool UnlockPart(PerkPart perkPart)
+	{
+		switch (perkPart) 
+		{
+		case PerkPart.Main:
+			if (!mMainUnlocked)
+			{
+				mMainUnlocked = true;
+				return true;
+			}
+			break;
+		case PerkPart.Left:
+			if (mMainUnlocked && (!mLeftUnlocked))
+			{
+				mLeftUnlocked = true;
+				return true;
+			}
+			break;
+		case PerkPart.Right:
+			if (mMainUnlocked && (!mRightUnlocked))
+			{
+				mRightUnlocked = true;
+				return true;
+			}
+			break;
+		default:
+			print("Error part in UnlockPart: " + perkPart);
+			break;
+		}
+		
+		return false;
+	}
+	
+	public override bool IsPartUnlocked(PerkPart perkPart)
+	{
+		switch (perkPart) 
+		{
+		case PerkPart.Main:
+			return mMainUnlocked;
+		case PerkPart.Left:
+			return mMainUnlocked && mLeftUnlocked;
+		case PerkPart.Right:
+			return mMainUnlocked && mRightUnlocked;
+		default:
+			print("Error part in IsPartUnlocked: " + perkPart);
+			break;
+		}
+		
+		return false;
+	}
+}
