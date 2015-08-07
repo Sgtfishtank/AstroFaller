8ae2482a8f3677cb97fab19e4041bc6848c8b967
Thism2 <thism.rasse@hotmail.com>
2015-08-07 15:06:31 +0200

    Added items scripts.

---
+++ Assets/Scripts/Game Menu/Items/BoltMagnet.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoltMagnet : Item 
+{
+	private bool mUnlocked;
+	private int mItemLevel;
+	
+	// Use this for initialization
+	void Start () 
+	{
+	}
+
+	public override void Init()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	
+	public override bool UnlockItem()
+	{
+		if (!mUnlocked)
+		{
+			mUnlocked = true;
+			mItemLevel = Mathf.Min(GlobalVariables.Instance.ITEMS_START_LEVEL, GlobalVariables.Instance.ITEMS_MAX_LEVEL);
+			return true;
+		}
+		else if (mItemLevel < GlobalVariables.Instance.ITEMS_MAX_LEVEL)
+		{
+			mItemLevel++;
+			return true;
+		}
+		
+		return false;	
+	}
+	
+	public override bool IsUnlocked()
+	{
+		return mUnlocked;
+	}
+	
+	public override int ItemLevelUnlocked()
+	{
+		if (!mUnlocked)
+		{
+			return -1;
+		}
+		
+		return mItemLevel;
+	}
+	
+	public override bool CanUnlockItem()
+	{
+		return ((!mUnlocked) || (mItemLevel < GlobalVariables.Instance.ITEMS_MAX_LEVEL));
+	}
+	
+	public override int BuyCostBolts()
+	{
+		switch (mItemLevel) 
+		{
+		default:
+			print ("Error perkPart in BuyCostBolts " + mItemLevel);
+			break;
+		}
+		
+		return -1;
+	}
+	
+	public override int BuyCostCrystals()
+	{
+		switch (mItemLevel) 
+		{
+		default:
+			print ("Error perkPart in BuyCostCrystals " + mItemLevel);
+			break;
+		}
+		
+		return -1;
+	}
+	
+	public override string BuyDescription()
+	{
+		return "---";
+	}
+	
+	public override string BuyCurrent()
+	{
+		return "---";
+	}
+	
+	public override string BuyNext()
+	{
+		return "---";
+	}
+}
