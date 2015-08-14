7af288a42146c9bd30702fe840b9c1e4b9f45b71
Thism2 <thism.rasse@hotmail.com>
2015-08-14 15:26:10 +0200

    Added InGameCamera and moved a material.

---
+++ Assets/Scripts/InGame/InGameCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGameCamera : MonoBehaviour 
+{
+	// snigleton
+	private static InGameCamera instance = null;
+	public static InGameCamera Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				GameObject thisObject = GameObject.Find("InGame Camera");
+				instance = thisObject.GetComponent<InGameCamera>();
+			}
+			return instance;
+		}
+	}
+	
+	// Use this for initialization
+	void Start () 
+	{
+		InGameCamera.Instance.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+}
