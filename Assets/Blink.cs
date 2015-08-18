a9405d034803e3a0549f7f40fe57ff7c3cb60aee
Chadista <a13ricir@HS-S-D30504.hs.local>
2015-08-18 17:16:23 +0200

    BlinkingLight

---
+++ Assets/Blink.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blink : MonoBehaviour 
+{
+	public Light mLight;
+	public float mBlinkTime;
+	public float mTime;
+
+	// Use this for initialization
+	void Start () 
+	{
+		mLight = GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Time.time > mTime)
+		{
+			mTime += mBlinkTime;
+			mLight.enabled = !mLight.enabled;
+		}
+	}
+}
