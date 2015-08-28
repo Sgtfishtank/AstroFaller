323ff1783a70f204373bac858d90c85a408d0973
Thism2 <thism.rasse@hotmail.com>
2015-08-28 18:32:24 +0200

    Added Segments.

---
+++ Assets/Scripts/Segment.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Segment : MonoBehaviour 
+{
+	public int mStaticWeight;
+	public int mLength;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
