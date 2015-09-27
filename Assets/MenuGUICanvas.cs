eac5e24c93de232fc262bed81660ece0f02041a0
Thism2 <thism.rasse@hotmail.com>
2015-09-27 14:15:24 +0200

    Updated prefabs, new GUICanvas.

---
+++ Assets/MenuGUICanvas.cs
@@ -0,0 +1,219 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuGUICanvas : MonoBehaviour 
+{
+	private GameObject mMenuButtons;
+	private GameObject mWorldMapButton;
+	private GameObject mPopupBuyMenu;
+	private GameObject mPopupCraftingMenu;
+	private GameObject mPopupAchievementsMenu;
+	private GameObject mItemButtons;
+	private GameObject mPerkButtons;
+	private GameObject mIconButtons;
+	private GameObject mPlayLevelButton;
+	
+	private Image mFadeImage;
+
+	private bool mShowButtons;
+	private Button[] mButtons;
+	private ButtonPress[] mButtonPresss;
+	
+	void Awake () 
+	{
+		mFadeImage = transform.Find ("FadeLayer").GetComponent<Image> ();
+		
+		//assign all menu buttons
+		mMenuButtons = transform.Find ("MenuButtons").gameObject;
+		mWorldMapButton = mMenuButtons.transform.Find ("WorldMapButton").gameObject;
+		mPopupBuyMenu = mMenuButtons.transform.Find ("PopupBuyMenu").gameObject;
+		mPopupCraftingMenu = mMenuButtons.transform.Find ("PopupCraftingMenu").gameObject;
+		mPopupAchievementsMenu = mMenuButtons.transform.Find ("PopupAchievementsMenu").gameObject;
+		mItemButtons = mMenuButtons.transform.Find ("Items").gameObject;
+		mPerkButtons = mMenuButtons.transform.Find ("Perks").gameObject;
+		mIconButtons = mMenuButtons.transform.Find ("Icons").gameObject;
+		mPlayLevelButton = mMenuButtons.transform.Find ("PlayLevelButton").gameObject;
+
+		mButtonPresss = GetComponentsInChildren<ButtonPress>(true);
+		mButtons = GetComponentsInChildren<Button>(true);
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		for (int i = 0; i < mButtonPresss.Length; i++) 
+		{
+			mButtonPresss[i].Init();	
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	
+	public Button[] GetButtons ()
+	{
+		return mButtons;
+	}
+
+	public void SetFadeColor(Color col)
+	{
+		mFadeImage.color = col;
+	}
+
+	// pressed buy perks
+	public void BuyAirPerk(int partperk)
+	{
+		MainGameMenu.Instance.PerksMenu().BuyAirPerk((Perk.PerkPart)partperk);
+	}
+	
+	public void BuyBurstPerk(int partperk)
+	{
+		MainGameMenu.Instance.PerksMenu().BuyBurstPerk((Perk.PerkPart)partperk);
+	}
+	
+	public void BuyLifePerk(int partperk)
+	{
+		MainGameMenu.Instance.PerksMenu().BuyLifePerk((Perk.PerkPart)partperk);
+	}
+	
+	// pressed buy items
+	public void BuyUlimitedAirItem()
+	{
+		MainGameMenu.Instance.ItemsMenu().BuyUlimitedAirItem();
+	}
+	
+	public void BuyShockwaveItem()
+	{
+		MainGameMenu.Instance.ItemsMenu().BuyShockwaveItem();
+	}
+	
+	public void BuyMagnetsItem()
+	{
+		MainGameMenu.Instance.ItemsMenu().BuyBoltMagnetItem();
+	}
+	
+	public void BuyForceFieldItem()
+	{
+		MainGameMenu.Instance.ItemsMenu().BuyForceFieldItem();
+	}
+	
+	public void BuyMultiplierItem()
+	{
+		MainGameMenu.Instance.ItemsMenu().BuyBoltMultiplierItem();
+	}
+	
+	public void BuyRocketThrustItem()
+	{
+		MainGameMenu.Instance.ItemsMenu().BuyRocketThrustItem();
+	}
+	
+	// pressed change world menus
+	public void ChangeToWorldMapMenu()
+	{
+		MainGameMenu.Instance.ChangeToWorldMapMenu();
+	}
+	
+	public void ChangeToPerksMenu()
+	{
+		MainGameMenu.Instance.ChangeToPerksMenu();
+	}
+	
+	public void ChangeToItemsMenu()
+	{
+		MainGameMenu.Instance.ChangeToItemsMenu();
+	}
+	
+	public void ChangeToChrystalShopMenu()
+	{
+		MainGameMenu.Instance.ChangeToChrystalShopMenu();
+	}
+	
+	// pressed main gui buttons
+	public void ToggleOptions ()
+	{
+		MainGameMenu.Instance.ToggleOptions();
+	}
+	
+	public void ToggleHelp ()
+	{
+		MainGameMenu.Instance.ToggleHelp();
+	}
+	
+	public void ToggleCraftingMenu ()
+	{
+		MainGameMenu.Instance.ToggleCraftingMenu();
+	}
+	
+	public void ToggleAchievementsMenu ()
+	{
+		MainGameMenu.Instance.ToggleAchievementsMenu();
+	}
+	
+	// pressed popup buy buttons
+	public void BuyWithBolts()
+	{
+		MainGameMenu.Instance.BuyWithBolts ();
+	}
+	
+	public void BuyWithCrystals()
+	{
+		MainGameMenu.Instance.BuyWithCrystals();
+	}
+	
+	// PlayLevel
+	public void PlayLevel()
+	{
+		MainGameMenu.Instance.WorldMapMenu().PlayLevel();
+	}
+
+	// get buttons
+	public ButtonPress PlayButton()
+	{
+		return mPlayLevelButton.GetComponent<ButtonPress>();
+	}
+
+	public void ShowPlayLevelButton (bool show)
+	{
+		mPlayLevelButton.gameObject.SetActive (show);
+	}
+	
+	public void ShowIconButtons(bool show)
+	{
+		mIconButtons.gameObject.SetActive (show);
+	}
+	
+	public void ShowWorldMapButton (bool show)
+	{
+		mWorldMapButton.gameObject.SetActive (show);
+	}
+	
+	public void ShowPopupBuyButton (bool show)
+	{
+		mPopupBuyMenu.gameObject.SetActive (show);
+	}
+	
+	public void ShowPopupCraftingButton (bool show)
+	{
+		mPopupCraftingMenu.gameObject.SetActive (show);
+	}
+	
+	public void ShowPopupAchievementsButton (bool show)
+	{
+		mPopupAchievementsMenu.gameObject.SetActive (show);
+	}
+	
+	public void ShowItemButtons (bool show)
+	{
+		mItemButtons.gameObject.SetActive (show);
+	}
+	
+	public void ShowPerkButtons (bool show)
+	{
+		mPerkButtons.gameObject.SetActive (show);
+	}
+
+}
---
+++ Assets/OptionsGUICanvas.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OptionsGUICanvas : MonoBehaviour 
+{
+	private GameObject mOptionButtons;
+	private GameObject mMasterButtons;
+	private GameObject mSoundButtons;
+	private GameObject mMusicButtons;
+
+	private bool mShowButtons;
+	private Button[] mButtons;
+	private ButtonPress[] mButtonPresss;
+	
+	void Awake () 
+	{
+		//assign all option buttons
+		mOptionButtons = transform.Find ("OptionButtons").gameObject;
+		mMasterButtons = mOptionButtons.transform.Find("Master").gameObject;
+		mSoundButtons = mOptionButtons.transform.Find("Sounds").gameObject;
+		mMusicButtons = mOptionButtons.transform.Find("Music").gameObject;
+		mOptionButtons.SetActive (true);
+
+		mButtonPresss = GetComponentsInChildren<ButtonPress>(true);
+		mButtons = GetComponentsInChildren<Button>(true);
+	}
+	
+	// Use this for initialization
+	void Start () 
+	{
+		UpdateOptions ();
+		
+		for (int i = 0; i < mButtonPresss.Length; i++) 
+		{
+			mButtonPresss[i].Init();	
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	
+	public Button[] GetButtons ()
+	{
+		return mButtons;
+	}
+
+	// buttons
+	public GameObject OptionButtons()
+	{
+		return mOptionButtons;
+	}
+	
+	// options
+	public void MuteMaster()
+	{
+		AudioManager.Instance.MuteMaster(mMasterButtons.transform.Find("Mute").GetComponent<Toggle>().isOn);
+	}
+	
+	public void MuteSounds()
+	{
+		AudioManager.Instance.MuteSounds(mSoundButtons.transform.Find("Mute").GetComponent<Toggle>().isOn);
+	}
+	
+	public void MuteMusic()
+	{
+		AudioManager.Instance.MuteMusic(mMusicButtons.transform.Find("Mute").GetComponent<Toggle>().isOn);
+	}
+	
+	public void MasterLevel()
+	{
+		AudioManager.Instance.MasterLevel(mMasterButtons.transform.Find("Slider").GetComponent<Slider>().value);
+	}
+	
+	public void SoundsLevel()
+	{
+		AudioManager.Instance.SoundsLevel(mSoundButtons.transform.Find("Slider").GetComponent<Slider>().value);
+	}
+	
+	public void MusicLevel()
+	{
+		AudioManager.Instance.MusicLevel(mMusicButtons.transform.Find("Slider").GetComponent<Slider>().value);
+	}
+	
+	public void AcceptOptions()
+	{
+		MainGameMenu.Instance.ShowOptions (false, true);
+	}
+	
+	public void CancelOptions()
+	{
+		MainGameMenu.Instance.ShowOptions (false, false);
+	}
+	
+	//
+	public void UpdateOptions()
+	{
+		mMusicButtons.transform.Find("Mute").GetComponent<Toggle>().isOn = AudioManager.Instance.IsMuteMusic();
+		mSoundButtons.transform.Find("Mute").GetComponent<Toggle>().isOn = AudioManager.Instance.IsMuteSounds();
+		mMasterButtons.transform.Find("Mute").GetComponent<Toggle>().isOn = AudioManager.Instance.IsMuteMaster();
+		mMusicButtons.transform.Find("Slider").GetComponent<Slider>().value = AudioManager.Instance.GetMusicLevel();
+		mSoundButtons.transform.Find("Slider").GetComponent<Slider>().value = AudioManager.Instance.GetSoundsLevel();
+		mMasterButtons.transform.Find("Slider").GetComponent<Slider>().value = AudioManager.Instance.GetMasterLevel();
+	}
+}
