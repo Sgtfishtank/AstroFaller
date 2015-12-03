428af71eb2364d92309eccda0f6b2aee979e6c6c
Thism2 <thism.rasse@hotmail.com>
2015-12-03 15:24:31 +0100

    New and moved scripts.

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class Singleton<T>
+{
+    public static T CreateInstance(string path)
+    {
+        GameObject prefab = Resources.Load<GameObject>(path);
+        GameObject thisObject = GameObject.Find(prefab.name);
+        if (thisObject == null)
+        {
+            thisObject = GameObject.Instantiate<GameObject>(prefab);
+            thisObject.name = prefab.name;
+        }
+        return thisObject.GetComponent<T>();
+    }
+}
