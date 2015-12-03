3047b2002f57aa4661d6f661689c1c9f5d93d80e
Sgtfishtank <joh_lau@hotamil.com>
2015-12-03 16:57:50 +0100

    prefabs working missing colliders will be fixed

--- Assets/Scripts/Storm/LaserLine.cs
+++ Assets/Scripts/Storm/LaserLine.cs
@@ -1,15 +1,113 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
 
 public class LaserLine : MonoBehaviour {
+    public Material[] emisionmats;
+    public GameObject electricity;
+    public GameObject[] electricityBallEffect;
+    public GameObject colliderHolder;
+    public BoxCollider[] colliders;
+    public float delayTime;
+    public float randomDelay;
+    public float burstTime;
+    public float randomBurstTime;
+    float lastBurstTime;
+    float localTime;
+    public float scale;
+    public Color baseColor;
+    
+    float idelActive;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public int count;
+    bool increase = true;
+
+
+
+    // Use this for initialization
+    void Start () {
+        baseColor = new Color(0.3529412f, 0.8125762f, 1);
+        Transform[] temp1 = transform.GetComponentsInChildren<Transform>();
+        electricityBallEffect = temp1.Where(x => x.name == "cloud_effect").Select(x => x.gameObject).ToArray();
+        electricity = temp1.Where(x => x.name == "Electricity").Select(x => x.gameObject).ToArray()[0];
+        GameObject[] a = temp1.Where(x => x.name == "cloud_lp:cloud_lp").Select(x => x.gameObject).ToArray();
+        emisionmats = new Material[a.Length];
+        for (int i = 0; i < a.Length; i++)
+        {
+            emisionmats[i]= a[i].GetComponent<Renderer>().material;
+            emisionmats[i].SetColor("_EmissionColor", baseColor*0);
+        }
+        electricity.SetActive(false);
+        for (int i = 0; i < electricityBallEffect.Length; i++)
+        {
+            electricityBallEffect[i].SetActive(false);
+        }
+        colliderHolder = temp1.Where(x => x.name == "Colliders").Select(x => x.gameObject).ToArray()[0];
+        colliders = new BoxCollider[electricityBallEffect.Length];
+        List<GameObject> temp = electricity.GetComponent<DigitalRuby.ThunderAndLightning.LightningBoltPathScript>().LightningPath.List;
+        for (int i = 0; i < temp.Count; i++)
+        {
+            colliders[i] = colliderHolder.AddComponent<BoxCollider>();
+        }
+       // colliderHolder.AddComponent<BoxCollider>
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+	    if(lastBurstTime < Time.time)
+        {
+            localTime += Time.deltaTime;
+            if(idelActive > Time.time)
+            {
+                return;
+            }
+            if (idelActive < Time.time && count == 4)
+            {
+                for (int i = 0; i < emisionmats.Length; i++)
+                {
+                    emisionmats[i].SetColor("_EmissionColor", baseColor*0);
+                }
+                electricity.SetActive(false);
+                for (int i = 0; i < electricityBallEffect.Length; i++)
+                {
+                    electricityBallEffect[i].SetActive(false);
+                }
+                lastBurstTime = delayTime + Random.Range(0, randomDelay) + Time.time;
+                localTime = 0;
+                count = 0;
+            }
+            if (count == 3)
+            {
+                idelActive = burstTime + Random.Range(0, randomBurstTime)+Time.time;
+                electricity.SetActive(true);
+                for (int i = 0; i < electricityBallEffect.Length; i++)
+                {
+                    electricityBallEffect[i].SetActive(true);
+                }
+                count = 4;
+            }
+            
+            float emission = Mathf.PingPong(localTime * scale, 1.0f);
+
+            Color finalColor = baseColor * Mathf.LinearToGammaSpace(emission);
+            for (int i = 0; i < emisionmats.Length; i++)
+            {
+                emisionmats[i].SetColor("_EmissionColor", finalColor);
+            }
+            if (emission >= 0.902f && increase)
+            {
+                count++;
+                increase = false;
+            }
+            if(emission < 0.5f)
+            {
+                increase = true;
+            }
+
+        }
 	}
 }
